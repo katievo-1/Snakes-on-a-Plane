2d7e427e9317508949d907ef0dcd0b2d60055869
Benjamin Romig <romigbl@mail.uc.edu>
2020-08-05 17:20:03 -0400

    Geolocation API now displays the city name

--- geolocation.js
+++ geolocation.js
@@ -1,13 +1,25 @@
 let city = document.getElementById("city");
 
-navigator.geolocation.getCurrentPosition(success, error);
+const locationURL = 'http://ip-api.com/json/?fields=status,message,country,countryCode,region,city';
 
-    function success(position) {
-        console.log(position.coords.latitude)
-        console.log(position.coords.longitude)
-        city.innerHTML = "Latitude: " + position.coords.latitude + 
-        "<br>Longitude: " + position.coords.longitude;
-    }
-    function error(err) {
-        console.log(err)
-    }
+function getLocation() {
+
+    // Fetch the location data from API
+
+    let xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function() {
+        
+        if (this.readyState == 4 && this.status == 200) {
+    		let response = JSON.parse(this.responseText);
+    		if(response.status !== 'success') {
+			    console.log('query failed: ' + response.message);
+		    	return
+            }
+            
+            city.innerHTML = response.city + ", " + response.region;
+	    }
+    };
+    xhr.open('GET', locationURL, true);
+    xhr.send();
+}
