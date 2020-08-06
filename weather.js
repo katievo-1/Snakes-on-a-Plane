65ff135d9d9a33c20f998e150ade6bd9f033c038
Benjamin Romig <romigbl@mail.uc.edu>
2020-08-06 19:50:25 -0400

    Added comments, need to find current conditions

--- weather.js
+++ weather.js
@@ -1,14 +1,18 @@
 // 2213054 Cincinnati Location Key
+// see https://developer.accuweather.com/api-flow-diagram
+
 
 navigator.geolocation.getCurrentPosition(success, error);
     function success(position) {
         console.log("Latitude: " + position.coords.latitude + 
         " Longitude: " + position.coords.longitude);
 
+        // Uses latitude and longitude to find location key
         let locationGeoURL = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?&apikey=R9vyjjt7tucL6SgBngZFXeGFpXrfoYyg&q" + 
         position.coords.latitude + "%2C" + position.coords.longitude;
 
         let xhr = new XMLHttpRequest();
+
             xhr.onreadystatechange = function() {
 
                 if (xhr.readyState == XMLHttpRequest.DONE) {
@@ -17,9 +21,9 @@ navigator.geolocation.getCurrentPosition(success, error);
 			            console.log('query failed: ' + response.message);
 		    	        return
                     }
-                    console.log(response.key);
-                    return response.key
-	            }
+                    console.log(response.Key);
+                    return response.Key
+	            } // Use Current Conditions API
         };
         xhr.open('GET', locationGeoURL, true);
         xhr.send();
